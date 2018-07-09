e03200633ca1bba68c26e7bbf00c6e2201c719b4
Mohammad Sammour <40949069+mohsam97@users.noreply.github.com>
2018-07-09 21:33:36 +0300

    Update moves.lua

--- objects/moves.lua
+++ objects/moves.lua
@@ -1,9 +1,10 @@
 Move = Object:extend()
 
-function Move:new(name,power,nature,min,max)
+function Move:new(name,power,nature,min,max,pp)
 	self.name = name
 	self.power = power
 	self.nature = nature
 	self.min = min
 	self.max = max
+	self.pp=pp
 end
